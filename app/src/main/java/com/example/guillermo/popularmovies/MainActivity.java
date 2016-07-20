78b9484e0ff43cf37944d87e1f46201eb72ed8ca
GuilleGrande <gillegrande@gmail.com>
2016-07-20 18:27:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/guillermo/popularmovies/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.guillermo.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String MOVIEBROWSERFRAGMENT_TAG = "MBF_TAG";
+
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "CREATE");
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(LOG_TAG, "DESTROY");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(LOG_TAG, "START");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(LOG_TAG, "STOP");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(LOG_TAG, "PAUSE");
+    }
+}
